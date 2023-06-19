cf89db6a4d21f425963c56dcc5c0222786d336f5
snnehir <snnehir21@gmail.com>
2023-06-19 10:06:38 +0300

    product model is added

---
+++ ShoppingApp/ShoppingApp/Models/AddProductModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingApp.Models
+{
+    public class AddProductModel
+    {
+        [Required]
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        [Required]
+        public string ImageUrl { get; set; }
+        [Required]
+        public decimal UnitPrice { get; set; }
+        [Required]
+        public int CategoryId { get; set; }
+    }
+}
